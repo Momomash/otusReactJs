0d1eeb5a72871ffbd327db4bad087b16419961d2
Momomash <momomash110697@gmail.com>
2020-04-24 19:21:56 +0300

    task 4 - create field generator

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import HelloWorld from "./Helloworld";
-
-
-export default class App extends React.Component{
-    render(){
-        return <HelloWorld userName="Марк" />
-    }
-}
--- src/Helloworld.stories.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import Helloworld from "./Helloworld";
-
-export default { title: 'Helloworld' };
-
-export const HelloworldText = () => <Helloworld userName={"Momo"}/>;
-

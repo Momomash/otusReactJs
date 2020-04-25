82e67215b00d9d32c2b42ed0df265d094a416698
Momomash <momomash110697@gmail.com>
2020-04-25 14:13:50 +0300

    task 4 - fiix eslint and prettier

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
+import * as React from 'react';
 
 import styled from '@emotion/styled';
 
 enum CellStatus {
     Empty = 0,
     Living = 1,
-    Young = 2
+    Young = 2,
 }
 
 const Game = styled.div`
@@ -15,23 +15,21 @@ const Game = styled.div`
     align-items: center;
 `;
 const FieldContainer = styled.div`
-  background-color: white;
-  border: 2px solid gray;
-  width: max-content;
+    background-color: white;
+    border: 2px solid gray;
+    width: max-content;
 `;
-const Controls = styled.div`
-
-`;
+const Controls = styled.div``;
 
 const FieldRow = styled.div`
     display: flex;
     flex-direction: row;
 `;
 const EmptyCell = styled.div`
-   background-color: white;
-   border: 1px solid lightgray;
-   width: 20px;
-   height: 20px;
+    background-color: white;
+    border: 1px solid lightgray;
+    width: 20px;
+    height: 20px;
 `;
 const LivingCell = styled.div`
     background-color: green;
@@ -47,9 +45,9 @@ const YongCell = styled.div`
 `;
 type Props = {};
 type State = {
-    sizeX: number,
-    sizeY: number,
-    cells: any,
+    sizeX: number;
+    sizeY: number;
+    cells: any;
 };
 
 export class Field extends React.Component<Props, State> {
@@ -58,7 +56,7 @@ export class Field extends React.Component<Props, State> {
         this.state = {
             sizeX: 10,
             sizeY: 10,
-            cells: this.generateCells(10, 10)
+            cells: this.generateCells(10, 10),
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -68,16 +66,18 @@ export class Field extends React.Component<Props, State> {
         const target = event.target;
         const name: string = target.name;
 
-        this.setState({...this.state, [name]: event.target.value});
+        this.setState({ ...this.state, [name]: event.target.value });
     }
 
     handleSubmit(event: any) {
-        this.setState({cells: this.generateCells(this.state.sizeX, this.state.sizeY)});
+        this.setState({
+            cells: this.generateCells(this.state.sizeX, this.state.sizeY),
+        });
         event.preventDefault();
     }
 
     generateCells(x: number, y: number): Array<Array<CellStatus>> {
-        let arr: Array<Array<CellStatus>> = [];
+        const arr: Array<Array<CellStatus>> = [];
         for (let i = y; i > 0; i--) {
             arr[i] = [];
             for (let j = x; j > 0; j--) {
@@ -85,7 +85,7 @@ export class Field extends React.Component<Props, State> {
             }
         }
         return arr;
-    };
+    }
 
     render() {
         return (
@@ -94,32 +94,44 @@ export class Field extends React.Component<Props, State> {
                 <FieldContainer>
                     <Controls>
                         <form onSubmit={this.handleSubmit}>
-                            <input type="number" min="10" value={this.state.sizeX} name="sizeX"
-                                   onChange={this.handleChange}
-                                   placeholder="X"/>
-                            <input type="number" min="10" value={this.state.sizeY} name="sizeY"
-                                   onChange={this.handleChange}
-                                   placeholder="Y"/>
-                            <input type="submit" placeholder="Generate Field" value="Generate field"
-                                   />
+                            <input
+                                type="number"
+                                min="10"
+                                value={this.state.sizeX}
+                                name="sizeX"
+                                onChange={this.handleChange}
+                                placeholder="X"
+                            />
+                            <input
+                                type="number"
+                                min="10"
+                                value={this.state.sizeY}
+                                name="sizeY"
+                                onChange={this.handleChange}
+                                placeholder="Y"
+                            />
+                            <input
+                                type="submit"
+                                placeholder="Generate Field"
+                                value="Generate field"
+                            />
                         </form>
                     </Controls>
-                    {this.state.cells.map((i: Array<number>) =>
+                    {this.state.cells.map((i: Array<number>) => (
                         <FieldRow>
                             {i.map((j: number) => {
-                                if (j == CellStatus.Empty) {
-                                    return <EmptyCell/>
-                                } else if (j == CellStatus.Living) {
-                                    return <LivingCell/>
+                                if (j === CellStatus.Empty) {
+                                    return <EmptyCell/>;
+                                } else if (j === CellStatus.Living) {
+                                    return <LivingCell/>;
                                 } else {
-                                    return <YongCell/>
+                                    return <YongCell/>;
                                 }
                             })}
-                        </FieldRow>)
-                    }
+                        </FieldRow>
+                    ))}
                 </FieldContainer>
             </Game>
         );
     }
 }
-

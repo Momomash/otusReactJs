5a3a978b84be69490f7d497c43ad58ed13afd84c
Momomash <momomash110697@gmail.com>
2020-05-16 16:51:09 +0300

    start tests

--- src/components/Field/Field.test.tsx
+++ src/components/Field/Field.test.tsx
@@ -4,16 +4,15 @@ import { mount, shallow } from 'enzyme';
 import { Field } from './Field';
 
 describe('Field', () => {
+    const wrapper = shallow(<Field />);
+    const sizeXInput = wrapper.find('input[name="sizeX"]');
+    const sizeYInput = wrapper.find('input[name="sizeY"]');
     it('check states with default sizex Field', () => {
-        const wrapper = shallow(<Field />);
         expect(wrapper.state('sizeX')).toBe(10);
         expect(wrapper.state('sizeY')).toBe(10);
     });
 
     it('check states with custom sizeX and sizeY after checked submit', () => {
-        const wrapper = shallow(<Field />);
-        const sizeXInput = wrapper.find('input[name="sizeX"]');
-        const sizeYInput = wrapper.find('input[name="sizeY"]');
         sizeXInput.simulate('change', { target: { value: 15 } });
         sizeYInput.simulate('change', { target: { value: 20 } });
         setTimeout(() => {

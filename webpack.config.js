00f25d9744c264add4efdccbe9da3cc49ab809df
Momomash <momomash110697@gmail.com>
2020-05-27 12:48:31 +0300

    install router, add alias '@/', create app.tsx

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,26 @@
-const path = require("path");
+const path = require('path');
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.tsx",
-    devtool: "source-map",
+    entry: './src/index.tsx',
+    devtool: 'source-map',
     resolve: {
-        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
     },
+
     output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "index.js",
+        path: path.join(__dirname, '/dist'),
+        filename: 'index.js',
     },
     module: {
         rules: [
             {
                 test: /\.(js|ts)x?$/,
-                loader: "babel-loader",
+                loader: 'babel-loader',
                 exclude: /node_modules/,
             },
         ],
@@ -25,9 +29,8 @@ module.exports = {
         historyApiFallback: true,
     },
     plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
-
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
 };

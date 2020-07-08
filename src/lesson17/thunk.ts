94a1b0fe105574b15f57d48f0933cd8ad3bbd0f5
Momomash <momomash110697@gmail.com>
2020-07-08 13:35:42 +0300

    remove comments

--- src/lesson17/thunk.ts
+++ src/lesson17/thunk.ts
@@ -1,11 +1,3 @@
-/*
-Курс React, урок 17: Middlewares
-Домашнее задание 2
-src/lesson17/homework/thunk.ts
-Напишите свой thunk middleware и подключите в приложение
-+1 балл за свой thunk middleware и подключение в приложение
-+1 балл за тесты
-*/
 import { Dispatch } from 'redux';
 
 export const getSwapiForCurrentPeople = () => (dispatch: Dispatch, getState: Function) => {
